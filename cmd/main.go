0cd9ce7d436db342e9ec9f992ad2cf2269243f98
Mateusz Szymczyk <mszymczyk-ext@saudidigitalbank.com>
2023-10-02 11:39:18 +0200

    delete opentelemetry completly

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,12 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/ds4tech/covantis-sre/pkg/common"
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/exporters/jaeger"
-	"go.opentelemetry.io/otel/sdk/resource"
-	tracesdk "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
 )
 
 const (
@@ -24,69 +16,23 @@ const (
 	port        = ":9090"
 )
 
-// tracerProvider returns an OpenTelemetry TracerProvider configured to use
-// the Jaeger exporter that will send spans to the provided url. The returned
-// TracerProvider will also use a Resource configured with all the information
-// about the application.
-func tracerProvider(url string) (*tracesdk.TracerProvider, error) {
-	// Create the Jaeger exporter
-	exp, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(url)))
-	if err != nil {
-		return nil, err
-	}
-	tp := tracesdk.NewTracerProvider(
-		// Always be sure to batch in production.
-		tracesdk.WithBatcher(exp),
-		// Record information about this application in a Resource.
-		tracesdk.WithResource(resource.NewWithAttributes(
-			semconv.SchemaURL,
-			semconv.ServiceNameKey.String(service),
-			attribute.String("environment", environment),
-			attribute.Int64("ID", id),
-		)),
-	)
-	return tp, nil
-}
-
 func main() {
-	tp, err := tracerProvider("http://localhost:14268/api/traces")
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	// Register our TracerProvider as the global so any imported
 	// instrumentation in the future will default to using it.
 	fmt.Println("Registering TracerProvider")
-	otel.SetTracerProvider(tp)
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	// tr := tp.Tracer("component-main")
 
-	// Cleanly shutdown and flush telemetry when the application exits.
-	defer func(ctx context.Context) {
-		// Do not make the application hang when it is shutdown.
-		ctx, cancel = context.WithTimeout(ctx, time.Second*5)
-		defer cancel()
-		if err := tp.Shutdown(ctx); err != nil {
-			log.Fatal(err)
-		}
-	}(ctx)
-
-	tr := tp.Tracer("component-main")
-
-	ctx, span := tr.Start(ctx, "foo")
-	defer span.End()
-
-	webserver(ctx)
+	webserver()
 
 }
 
-func webserver(ctx context.Context) {
+func webserver() {
 	// Use the global TracerProvider.
-	tr := otel.Tracer("component-webserver")
-	_, span := tr.Start(ctx, "webserver")
-	span.SetAttributes(attribute.Key("testset").String("value"))
-	defer span.End()
+	// tr := otel.Tracer("component-webserver")
+	// _, span := tr.Start(ctx, "webserver")
+	// span.SetAttributes(attribute.Key("testset").String("value"))
+	// defer span.End()
 
 	//starting webserver
 	env := os.Getenv("ENV")
