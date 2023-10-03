8baf109bc4647e77626e175ee07d73f253b26c6d
Mateusz Szymczyk <mszymczyk-ext@saudidigitalbank.com>
2023-10-03 11:39:28 +0200

    package models

--- pkg/models/config.go
+++ pkg/models/config.go
@@ -1,24 +1,38 @@
 package models
 
 import (
-	"io/ioutil"
 	"encoding/json"
- )
- 
- type Config struct  {
-	Db map[string]string `json:"db"`
+	"io/ioutil"
+)
+
+var (
+	Configuration Config
+)
+
+init() {
+    args := os.Args[1:]
+    if len(args) == 0{
+       fmt.Println("********************\nMust specify a config file   in args\n********************")
+       os.Exit(1)
+   }
+
+   Configuration = NewConfig(args[0]) // configuration initialized here
+}
+
+type Config struct {
+	Db     map[string]string `json:"db"`
 	Server map[string]string `json:"server"`
- }
- 
- 
- func NewConfig(fname string) *Config{
-	data,err := ioutil.ReadFile(fname)
-	if err != nil{
-	   panic(err)
+}
+
+func NewConfig(fname string) *Config {
+	data, err := ioutil.ReadFile(fname)
+	if err != nil {
+		panic(err)
 	}
 	config := Config{}
-	err = json.Unmarshal(data,&config)
+	err = json.Unmarshal(data, &config)
 	if err != nil {
-	panic(err)
- }
- return config
+		panic(err)
+	}
+	return config
+}
